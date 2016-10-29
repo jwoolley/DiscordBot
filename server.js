a1139f72742c330e4f9dff6d315b588d3bbce0df
Justin Woolley <jwoolley@netflix.com>
2016-10-29 12:18:38 -0700

    handle unknown users for roll stats

--- server.js
+++ server.js
@@ -916,13 +916,13 @@ Promise.all(configs.map(config => loadConfig(config))).then(() => {
             if (userAggregate.mostRolls === undefined || rolls.length > userAggregate.mostRolls.value) {
               userAggregate.mostRolls = { user: user, value: rolls.length };
             }
-            if (userAggregate.lowestAverage === undefined || rolls.length >= minRollsForAverage && averageRoll < userAggregate.lowestAverage.value) {
+            if (userAggregate.lowestAverage === undefined || user.username != '**unknown user**' && rolls.length >= minRollsForAverage && averageRoll < userAggregate.lowestAverage.value) {
               userAggregate.lowestAverage = { user: user, value: averageRoll };
             }
-            if (userAggregate.highestAverage === undefined || rolls.length >= minRollsForAverage && averageRoll > userAggregate.highestAverage.value) {
+            if (userAggregate.highestAverage === undefined || user.username != '**unknown user**' && rolls.length >= minRollsForAverage && averageRoll > userAggregate.highestAverage.value) {
               userAggregate.highestAverage = { user: user, value: averageRoll };
             }
-            if (userAggregate.averageAverage === undefined || rolls.length >= minRollsForAverage && Math.abs(size/2 - averageRoll) < Math.abs(size/2 - userAggregate.averageAverage.value)) {
+            if (userAggregate.averageAverage === undefined || user.username != '**unknown user**' && rolls.length >= minRollsForAverage && Math.abs(size/2 - averageRoll) < Math.abs(size/2 - userAggregate.averageAverage.value)) {
               userAggregate.averageAverage = { user: user, value: averageRoll };
             }                  
             return userAggregate;
